eb6d57a36c61d1b97eb004b3fa09e32d9a53369d
prozet <abcd-xyz@list.ru>
2023-05-17 00:42:30 +0700

    st 2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,30 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
 
 public class App {
-    public static void App(String[] args) {
-        Javalin.create();
+    public static void main(String[] args) {
+        Javalin app = getapp();
+        app.start(getPort());
+    }
+
+    public static void addRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.result("Hello, world!"));
+    }
+
+    private static Integer getPort() {
+        String port = System.getenv().getOrDefault("PORT", "8080");
+        return Integer.valueOf(port);
+    }
+
+    public static Javalin getapp() {
+        var app = Javalin.create(config -> {
+            config.enableDevLogging();
+        });
+
+        addRoutes(app);
+
+        return app;
     }
 }
