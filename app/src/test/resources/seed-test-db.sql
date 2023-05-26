e346e4a1e12f0ec4961e7c640fa7e24ccb959638
Dmitriy Nesterovich <abcd-xyz@list.ru>
2023-05-26 19:22:07 +0700

    st 4

---
+++ app/src/test/resources/seed-test-db.sql
@@ -0,0 +1,18 @@
+INSERT INTO url (name, created_at) VALUES
+('The Man Within', '2022-01-01 13:57:40'),
+('Consider the Lilies', '2022-01-01 14:57:40'),
+('The Lathe of Heaven', '2022-02-21 12:00:40'),
+('Clouds of Witness', '2022-02-21 12:00:40'),
+('Eyeless in Gaza', '2022-02-21 12:00:40'),
+('Oh! To be in England', '2022-02-21 12:00:40'),
+('A Passage to India', '2022-02-21 12:00:40'),
+('Tender Is the Night', '2022-02-21 12:00:40'),
+('The Last Enemy', '2022-02-21 12:00:40'),
+('I Know Why the Caged Bird Sings', '2022-02-21 12:00:40'),
+('East of Eden', '2022-02-21 12:00:40'),
+('The Moving Toyshop', '2022-02-21 12:00:40'),
+('Death Be Not Proud', '2022-02-21 12:00:40'),
+('Dying of the Light', '2022-02-21 12:00:40'),
+('The Monkey`s Raincoat', '2022-02-21 12:00:40'),
+('The World, the Flesh and the Devil', '2022-02-21 12:00:40'),
+('To a God Unknown', '2022-02-21 12:00:40');
