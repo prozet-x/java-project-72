1fe4a00f7dc8ad734917c431c01adb046f6959eb
prozet <abcd-xyz@list.ru>
2023-06-10 01:15:27 +0700

    st 5

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void testInit() {
+        assertTrue(false);
+    }
+}
