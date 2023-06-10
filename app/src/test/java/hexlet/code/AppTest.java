dc2f6620cf8848542de0f24bfdd7a083c8bf91f5
prozet <abcd-xyz@list.ru>
2023-06-11 02:34:28 +0700

    st 5

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,21 +1,38 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
-import org.apache.http.entity.mime.content.StringBody;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
+    private static Javalin app;
+    private static String baseUrl;
+
     @Test
-    public void testInit() {
-        assertTrue(false);
+    void testInit() {
+        assertThat(true).isTrue();
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        app = App.getApp();
+        app.start();
+        baseUrl = String.format("http://localhost:%d", app.port());
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        app.stop();
     }
 
     @Test
-    public void mainPage() {
-        HttpResponse<String> response = Unirest.get("http://localhost:8080/").asString();
-        response.getBody().
+    void mainPage() {
+        HttpResponse<String> response = Unirest.get(baseUrl).asString();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getBody().toString()).contains("Бесплатно проверяйте сайты на SEO пригодность");
     }
 }
