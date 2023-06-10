fed4a2dfd42baa8f8d6d6f53badcc47b4fe173c9
prozet <abcd-xyz@list.ru>
2023-06-11 00:52:59 +0700

    st 5

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,11 +1,21 @@
 package hexlet.code;
 
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.apache.http.entity.mime.content.StringBody;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
     @Test
     public void testInit() {
         assertTrue(false);
+    }
+
+    @Test
+    public void mainPage() {
+        HttpResponse<String> response = Unirest.get("http://localhost:8080/").asString();
+        response.getBody().
     }
 }
